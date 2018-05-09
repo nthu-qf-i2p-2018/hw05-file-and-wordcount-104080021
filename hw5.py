8128738a03b3c0a7cb2d88413842cb56e849c501
104080021 <37204177+104080021@users.noreply.github.com>
2018-05-09 21:51:28 +0800

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -1,35 +1,34 @@
 # -*- coding: utf-8 -*-
 # 104080021-hw5
-
-import csv, json, pickle, string
+from collections import Counter
+import csv
+import json
+import pickle
+import string
 
 
 def main(filename):
-    txtfile = open(filename)
-    lines = txtfile.readline()
     all_words = []
-    
-    for line in lines:
-        words = line.split()
 
-        for word in words:
+    for line in open(filename):
+        line = line.strip()
+        if not line:
+            continue
+        for word in line.split():
             word = word.strip(string.punctuation)
-            
             if word:
                 all_words.append(word)
 
-    from collections import counter
-    counter = counter(all_words)
+    counter = Counter(all_words)
 
-    with open("wordcount.csv", "w") as csv_file:
+    with open("wordcount.csv", "w", newline="") as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(['word', 'count'])
         writer.writerows(counter.most_common())
-        
-    with open("wordcount.json", "w") as json_file:
-        json.dump(counter, json_file)
-        
-    pickle.dump(counter, open("wordcount.pkl", 'wb'))
-    
+
+    json.dump(list(counter.most_common()), open("wordcount.json", "w"))
+
+    pickle.dump(counter, open("wordcount.pkl", "wb"))
+
 if __name__ == '__main__':
     main("i_have_a_dream.txt")
